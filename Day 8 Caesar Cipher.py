7eb20a5ecf3a356ac5c9eb112a8ce39929ee1913
Paulus Germanus <paulus.germanus@gmail.com>
2023-06-16 21:02:25 +0200

    day 8 additional functionality

--- Day 8 Caesar Cipher.py
+++ Day 8 Caesar Cipher.py
@@ -3,8 +3,8 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
 shift = int(input("Type the shift nubmer:\n"))
 
 shift %= 26
@@ -12,13 +12,23 @@ shift %= 26
 def encrypt(input_text, shift_value):
     encrypted_text = ""
     for x in input_text:
-        encrypted_text += alphabet[alphabet.index(x)+shift_value]
+        if x == " ":
+            encrypted_text += x
+        elif x.isnumeric():
+            encrypted_text += x
+        else:
+            encrypted_text += alphabet[alphabet.index(x)+shift_value]
     print(f"Encrypted text: {encrypted_text}")
 
 def decrypt(input_text, shift_value):
     decrypted_text = ""
     for x in input_text:
-        decrypted_text += alphabet[alphabet.index(x)-shift_value]
+        if x == " ":
+            decrypted_text += x
+        elif x.isnumeric():
+            decrypted_text += x
+        else:
+            decrypted_text += alphabet[alphabet.index(x)+shift_value]
     print(f"Decrypted text: {decrypted_text}")
 
 if direction == "encode":
