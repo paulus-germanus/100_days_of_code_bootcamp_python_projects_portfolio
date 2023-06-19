c0148103eb3c7681f16b91284fefc4587dc98ba2
Paulus Germanus <paulus.germanus@gmail.com>
2023-06-19 18:45:54 +0200

    day 10 small project

---
+++ Day 10 Days in a Month.py
@@ -0,0 +1,16 @@
+def is_leap(year):
+    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        return True
+    elif year % 4 == 0 and year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    else:
+        return month_days[month-1]
+
+print(days_in_month(year=int(input("Enter the year.\n")), month=int(input("Enter the month to check how many day's it has.\n"))))
