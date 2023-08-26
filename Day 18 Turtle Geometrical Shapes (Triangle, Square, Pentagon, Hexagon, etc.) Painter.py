d303031fb9a8abd2eaa999e38d254bb68e310532
Paulus Germanus <paulus.germanus@gmail.com>
2023-08-26 14:20:57 +0200

    Update Day 18 Turtle Geometrical Shapes (Triangle, Square, Pentagon, Hexagon, etc.) Painter.py
    random color generator function

--- Day 18 Turtle Geometrical Shapes (Triangle, Square, Pentagon, Hexagon, etc.) Painter.py
+++ Day 18 Turtle Geometrical Shapes (Triangle, Square, Pentagon, Hexagon, etc.) Painter.py
@@ -1,29 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+import turtle
 import random
 
-myColorList = ["red","green","black","purple","blue","goldenrod", "dodger blue"]
-
+turtle.colormode(255)
 t = Turtle()
 t.hideturtle()
-t.penup()
 t.speed(0)
-t.left(90)
-t.forward(250)
-t.left(90)
-t.forward(60)
-t.right(180)
+t.penup()
+t.setx(-55)
+t.sety(270)
 t.pendown()
+
+
+def random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
 
 x = 3
 
 while True:
-    t.pencolor(random.choice(myColorList))
+    t.pencolor(random_color())
     for z in range(x):
         t.forward(100)
         t.right(360/x)
     x += 1
-
-
-
-screen = Screen()
-screen.exitonclick()
